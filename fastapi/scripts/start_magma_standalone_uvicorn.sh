f0f76275686c278c20855e64404ad1c03c22cbd9
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2025-06-04 03:10:23 -0700

    simplified compose. viz improved with iframe. standardized main.py name.

--- fastapi/scripts/start_magma_standalone_uvicorn.sh
+++ fastapi/scripts/start_magma_standalone_uvicorn.sh
@@ -6,5 +6,5 @@
 #   The Compose stack will still be running so you have Postgres and pgAdmin, but you will bypass the running
 #   Bedrock container and connect to postgres from your locally-running Magma FastAPI app, started like this:
 
-uvicorn magmastart:app --reload
+uvicorn main:app --reload
 
